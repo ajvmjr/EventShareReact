f3410dc436014aa0fb12100ddcc01621e6d86fbb
FIC <FIC@info.local>
2019-12-11 09:25:07 -0300

    Perfil adm eventos para aprovar e parte do cadastrar evento 4

--- src/pages/PerfilAdmAprovarEventos.js
+++ src/pages/PerfilAdmAprovarEventos.js
@@ -1,80 +1,137 @@
 import React, { Component } from "react";
 import '../assets/CSS/PerfilAdmAprovarEventos.css';
 import Api from '../services/Api';
-import Cabecalho from '../components/CabecalhoSemBotao';
+import Cabecalho from '../components/CabecalhoBotao';
 import Rodape from '../components/Rodape';
 import TopoPerfil from '../components/TopoPerfil';
+import Moment from 'react-moment';
+import 'moment-timezone';
 
 class PerfilAdmAprovarEvento extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listaEventos: [],
+      listaCategorias: [],
+      listaStatus: [],
+      eventoNome: '',
+      categoriaId: '',
+      categoriaNome: '',
+      eventoStatusId: '',
+      eventoStatusNome: ''
+    }
+  }
+
+  buscarEvento() {
+    fetch('http://localhost:5000/api/eventotbl')
+      .then(resposta => resposta.json())
+      .then(data => this.setState({
+        listaEventos: data,
+        listaFiltradaDeEventos: data,
+        ListaDataFiltrada: data
+      }))
+      .catch((erro) => console.log(erro))
+  }
+
+  toggleFiltro(event) {
+    let id = event.target.value
+    this.filtrarStatus(id)
+    // console.log(id)
+  }
+
+  filtrarStatus(id) {
+    console.log('Entramos em filtrar status o id é: ' + id)
+    var listaEventos = this.state.listaEventos
+
+    var listaFiltrada = listaEventos.filter((value, index, arr) => {
+      if (listaEventos[index].eventoStatusId == id) {
+        return listaEventos[index]
+      }
+    })
+
+    this.setState({
+      listaFiltradaDeEventos: listaFiltrada
+    })
+  }
+
+
+  buscarStatus() {
+    this.setState({ loading: true })
+
+    fetch('http://localhost:5000/api/status')
+      .then(resposta => resposta.json())
+      .then(data => this.setState({ listaStatus: data }))
+      .catch((erro) => console.log(erro))
+  }
+
+  componentDidMount() {
+    this.buscarEvento()
+    this.buscarStatus()
+  }
+
   render() {
     return (
       <div>
-        <Cabecalho/>
-        <TopoPerfil/>
-        <main class="main-perfil-adm-aprovar-eventos">
-          <section class="titulo-perfil-adm-aprovar-eventos">
+        <Cabecalho />
+        <TopoPerfil />
+
+        <main class="main-aprovar-eventos">
+          <section class="titulo-aprovar-eventos">
             <h1>Aprovar Eventos</h1>
           </section>
+          <div className="filtro-status-data-aprovar">
 
-          <section id="eventos-perfil-adm-aprovar-eventos">
-            <div class="eventos-perfil-adm-aprovar-eventos">
-              <a href="#">
-                <div class="evento-1-perfil-adm-aprovar-eventos">
-                  <div class="evento-um-perfil-adm-aprovar-eventos">
-                    <img
-                      src={require("../assets/imagens/Evento1.jpeg")}
-                      alt=""
-                      class="evento-imagem-perfil-adm-aprovar-eventos"
-                    />
-                    <div class="evento-nome-data-hora-local-perfil-adm-aprovar-eventos">
-                      <div class="evento-nome-perfil-adm-aprovar-eventos">
-                        <p>WAKANDA TALKS</p>
-                      </div>
-                      <div class="evento-data-hora-local-perfil-adm-aprovar-eventos">
-                        <div class="data-evento-perfil-adm-aprovar-eventos">
-                          <p>07 Agosto</p>
-                        </div>
-                        <div class="hora-evento-perfil-adm-aprovar-eventos">
-                          <p>19:00</p>
-                        </div>
-                        <div class="espaco-evento-perfil-adm-aprovar-eventos">
-                          <p>Lounge</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </a>
+            <select onChange={this.toggleFiltro.bind(this)} value='value-select' id='id-select' className="filtro-status-aprovar" >
+              <option id="" selected>Selecione um status</option>
+              {
+                this.state.listaStatus.map(function (status) {
+                  return <option value={status.eventoStatusId} key={status.eventoStatusId} name="teste">{status.eventoStatusNome}</option>
+                })
+              }
+            </select>
+          </div>
 
-              <a href="#">
-                <div class="evento-2-perfil-adm-aprovar-eventos">
-                  <div class="evento-dois-perfil-adm-aprovar-eventos">
-                    <img
-                      src={require("../assets/imagens/Evento2.jpeg")}
-                      alt=""
-                      class="evento-imagem-perfil-adm-aprovar-eventos"
-                      />
-                    <div class="evento-nome-data-hora-local-perfil-adm-aprovar-eventos">
-                      <div class="evento-nome-perfil-adm-aprovar-eventos">
-                        <p>THOUGHTWORKS TECH TALKS</p>
-                      </div>
-                      <div class="evento-data-hora-local-perfil-adm-aprovar-eventos">
-                        <div>
-                          <p>16 Julho</p>
+
+          <section id="eventos-aprovar-eventos">
+            <div class="eventos-menor-aprovar-eventos">
+              {
+                this.state.listaFiltradaDeEventos ?
+                  this.state.listaFiltradaDeEventos.map(function (evento) {
+                    return (
+                      <a href="#">
+                        <div key={evento.eventoId} className="evento-1-aprovar">
+                          <div className="evento-um-aprovar">
+                            <img src={require("../assets/imagens/Evento1.jpeg")} alt="" className="evento-imagem-aprovar" />
+                            <div className="evento-nome-data-hora-local-aprovar">
+                              <div className="evento-nome-aprovar">
+                                <p>{evento.eventoNome}</p>
+                              </div>
+                              <div className="evento-data-hora-local-aprovar">
+                                <div className="data-evento-aprovar">
+                                  <p>Data:</p>
+                                  <Moment class='data-formato-aprovar' format="DD/MM/YYYY">
+                                    {evento.eventoData}
+                                  </Moment>
+                                </div>
+                                <div class="hora-evento-aprovar">
+                                  <p class='horario-bold-aprovar'>Horário:</p>
+                                  <p>{evento.eventoHorarioComeco}</p>
+                                </div>
+                                <div className="espaco-evento-aprovar">
+                                  <p className='espaco-aprovar'>Local:</p>
+                                  <p>{evento.eventoEspaco.espacoNome}</p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
                         </div>
-                        <div>
-                          <p>19:00</p>
-                        </div>
-                        <div>
-                          <p>Lounge</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </a>
+                      </a>
+
+                    )
+                  }) : null
+              }
+
             </div>
-
             <section id="secao-botao-ver-mais-perfil-adm-aprovar-eventos">
               <button class="botao-ver-mais-perfil-adm-aprovar-eventos">
                 Ver mais
@@ -82,7 +139,7 @@ class PerfilAdmAprovarEvento extends Component {
             </section>
           </section>
         </main>
-        <Rodape/>
+        <Rodape />
       </div>
     );
   }
